e99b1589656407461d62f748cd479cc9daed5533
ashishshetty95 <ashishshetty95@gmail.com>
2023-12-03 19:19:21 +0530

    g

--- src/test/java/Jenkins/Parameters.java
+++ src/test/java/Jenkins/Parameters.java
@@ -3,7 +3,6 @@ package Jenkins;
 import org.testng.annotations.Test;
 
 public class Parameters {
-	@org.testng.annotations.Parameters
 	@Test
 	public void demo() {
 		String un=System.getProperty("username");
