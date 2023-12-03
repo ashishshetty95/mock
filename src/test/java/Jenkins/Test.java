fc633759d2a45dfc4038525d760d28bc4c08ed80
ashishshetty95 <ashishshetty95@gmail.com>
2023-12-03 19:16:15 +0530

    Commit 5

--- src/test/java/Jenkins/Test.java
+++ src/test/java/Jenkins/Test.java
@@ -3,6 +3,8 @@ package Jenkins;
 public class Test {
 	@org.testng.annotations.Test
 	public void testA() {
+		String un=System.getProperty("username");
+		System.out.println(un);
 		System.out.println("Hi");
 		System.out.println("Hello");
 	}
