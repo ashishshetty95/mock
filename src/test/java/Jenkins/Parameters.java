db4bb313854295b804ebdfe1982de1f94683071e
ashishshetty95 <ashishshetty95@gmail.com>
2023-12-03 19:27:48 +0530

    d

--- src/test/java/Jenkins/Parameters.java
+++ src/test/java/Jenkins/Parameters.java
@@ -5,8 +5,7 @@ import org.testng.annotations.Test;
 public class Parameters {
 	@Test
 	public void demo() {
-		String un=System.getProperty("username");
-		System.out.println(un);
+		System.out.println("un");
 	}
 
 }
