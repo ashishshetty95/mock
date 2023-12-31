4fb6fb7dc2686cd9b0b9c9a5de0576e55a2084cd
ashishshetty95 <ashishshetty95@gmail.com>
2023-12-31 19:59:41 +0530

    k

--- src/test/java/Jenkins/OpenBrowserTest.java
+++ src/test/java/Jenkins/OpenBrowserTest.java
@@ -18,4 +18,18 @@ public class OpenBrowserTest {
 		driver.get("https://www.facebook.com");
 		System.out.println(driver.getTitle());
 	}
+	
+	@Test
+	public void chooseTest() {
+		String app = System.getProperty("website");
+		if (app.equalsIgnoreCase("facebook")) {
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://www.facebook.com");
+			System.out.println(driver.getTitle());
+		} else if (app.equalsIgnoreCase("google")) {
+			WebDriver driver=new ChromeDriver();
+			driver.get("https:www.google.com");
+			System.out.println(driver.getTitle());
+		}
+	}
 }
